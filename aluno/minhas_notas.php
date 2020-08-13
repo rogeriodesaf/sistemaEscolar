0095baf644781ed28f3ec9f9fa4373fc3629f9d9
Rogério de Sá <rogeriodesaf@gmail.com>
2020-08-13 15:33:52 -0300

    up

--- aluno/minhas_notas.php
+++ aluno/minhas_notas.php
@@ -135,13 +135,14 @@ $sql_1 = "SELECT * FROM disciplinas WHERE curso = '$serie'";
 
         $sql_2 = "SELECT * FROM notas_provas WHERE code = '$code' AND disciplina = '$disciplina' AND semestre = '1'";
         $result_2 = mysqli_query($conexao, $sql_2);
-        //$sql_3 = "SELECT * FROM notas_provas WHERE code = '$code' AND disciplina = '$disciplina' AND semestre = '2'";
+        //$sql_3 = "SELECT * FROM notas_trabalhos WHERE code = '$code' AND disciplina = '$disciplina' AND semestre = '2'";
         //$result_3 = mysqli_query($conexao, $sql_3);
-        // $sql_4 = "SELECT * FROM notas_provas WHERE code = '$code' AND disciplina = '$disciplina' AND semestre = '3'";
-        // $result_4 = mysqli_query($conexao, $sql_4);
-        // $sql_5 = "SELECT * FROM notas_provas WHERE code = '$code' AND disciplina = '$disciplina' AND semestre = '4'";
-        // $result_5 = mysqli_query($conexao, $sql_5);
-        //?>
+        //$sql_4 = "SELECT * FROM notas_trabalhos WHERE code = '$code' AND disciplina = '$disciplina' AND semestre = '3'";
+        //$result_4 = mysqli_query($conexao, $sql_4);
+        //$sql_5 = "SELECT * FROM notas_trabalhos WHERE code = '$code' AND disciplina = '$disciplina' AND semestre = '4'";
+        //$result_5 = mysqli_query($conexao, $sql_5);
+
+        ?>
             <tr>
                 <td><?php echo $disciplina; ?></td>
                 <td>
@@ -151,79 +152,62 @@ if (mysqli_num_rows($result_2) == '') {
         } else {
             while ($res_2 = mysqli_fetch_assoc($result_2)) {
                 $nota = $res_2['nota'];
-                $prova = $res_2['prova'];
-
                 if ($nota >= 7) {
-                    echo "<h2><strong>" . $res_2['nota'] . "</strong>";
+                    echo "<h2><strong>$nota</strong></h2>";
                 } else {
-                    echo "<h3><strong>" . $res_2['nota'] . "</strong>";
-                }
-                if ($res_2['prova'] == '') {
-                } else {
-                    echo " | <a target='_blank' class='a5' href='../trabalhos_alunos/$prova'>Ver</a></h2></h3>";
+                    echo "<h3><strong>$nota</strong></h3>";
                 }
 
-            }}?>
+            }
+        }?>
                 </td>
                 <td>
                     <?php
-if (mysqli_num_rows($result_3) == '') {
-            "<h2>Aguarda</h2>";
-        } else {
-            while ($res_3 = mysqli_fetch_assoc($result_3)) {
-                $nota = $res_3['nota'];
-                $prova = $res_3['prova'];
+//if (mysqli_num_rows($result_3) == '') {
+        //     echo "<h2>Aguarda</h2>";
+        // } else {
+        //   while ($res_3 = mysqli_fetch_assoc($result_3)) {
+        //    $nota = $res_3['nota'];
+        //  if ($nota >= 7) {
+        //     echo "<h2><strong>$nota</strong></h2>";
+        //  } else {
+        //      echo "<h3><strong>$nota</strong></h3>";
+        //   }
 
-                if ($nota >= 7) {
-                    "<h2><strong>" . $res_3['nota'] . "</strong>";
-                } else {
-                    "<h3><strong>" . $res_3['nota'] . "</strong></h3>";
-                }
-                if ($res_3['prova'] == '') {
-                } else {
-                    " | <a target='_blank' class='a5' href='../trabalhos_alunos/$prova'>Ver</a></h2>";
-                }
-            }}?>
+        // }
+        //  }?>
                 </td>
                 <td>
                     <?php
-if (mysqli_num_rows($result_4) == '') {
-            echo "<h2>Aguarda</h2>";
-        } else {
-            while ($res_4 = mysqli_fetch_assoc($result_4)) {
-                $nota = $res_4['nota'];
-                $prova = $res_4['prova'];
+//if (mysqli_num_rows($result_4) == '') {
+        //    echo "<h2>Aguarda</h2>";
+        // } else {
+        // while ($res_4 = mysqli_fetch_assoc($result_4)) {
+        // $nota = $res_4['nota'];
+        //  if ($nota >= 7) {
+        // echo "<h2><strong>$nota</strong></h2>";
+        //  } else {
+        //  echo "<h3><strong>$nota</strong></h3>";
+        //  }
 
-                if ($nota >= 7) {
-                    echo "<h2><strong>" . $res_4['nota'] . "</strong>";
-                } else {
-                    echo "<h3><strong>" . $res_4['nota'] . "</strong></h3>";
-                }
-                if ($res_4['prova'] == '') {
-                } else {
-                    echo " | <a target='_blank' class='a5' href='../trabalhos_alunos/$prova'>Ver</a></h2>";
-                }
-            }}?>
+        //}
+        // }?>
                 </td>
                 <td>
                     <?php
-if (mysqli_num_rows($result_5) == '') {
-            echo "<h2>Aguarda</h2>";
-        } else {
-            while ($res_5 = mysqli_fetch_assoc($result_5)) {
-                $nota = $res_5['nota'];
-                $prova = $res_5['prova'];
+//if (mysqli_num_rows($result_5) == '') {
+        //   echo "<h2>Aguarda</h2>";
+        //  } else {
+        // while ($res_5 = mysqli_fetch_assoc($result_5)) {
+        // $nota = $res_5['nota'];
+        // if ($nota >= 7) {
+        //   echo "<h2><strong>$nota</strong></h2>";
+        //   } else {
+        // echo "<h3><strong>$nota</strong></h3>";
+        // }
 
-                if ($nota >= 7) {
-                    echo "<h2><strong>" . $res_5['nota'] . "</strong>";
-                } else {
-                    echo "<h3><strong>" . $res_5['nota'] . "</strong></h3>";
-                }
-                if ($res_5['prova'] == '') {
-                } else {
-                    echo " | <a target='_blank' class='a5' href='../trabalhos_alunos/$prova'>Ver</a></h2>";
-                }
-            }}?>
+        // }
+        // }?>
                 </td>
             </tr>
             <?php }?>
@@ -241,7 +225,7 @@ if (mysqli_num_rows($result_5) == '') {
             <tr>
                 <td width="317"><strong>DISCIPLINA<br /><br /></strong></td>
                 <td width="150"><strong>1º Semestre</strong></td>
-                <td width="150"><strong>2º Semestre</strong></td>
+                <!-- <td width="150"><strong>2º Semestre</strong></td>
                 <!--<td width="150"><strong>3º Bimestre</strong></td>
     <td width="150"><strong>4º Bimestre</strong></td>-->
             </tr>
@@ -254,14 +238,14 @@ $sql_1 = "SELECT * FROM disciplinas WHERE curso = '$serie'";
         $sql_2 = "SELECT * FROM notas_observacao WHERE code = '$code' AND disciplina = '$disciplina' AND semestre = '1'";
         $result_2 = mysqli_query($conexao, $sql_2);
 
-        $sql_3 = "SELECT * FROM notas_observacao WHERE code = '$code' AND disciplina = '$disciplina' AND semestre = '2'";
-        $result_3 = mysqli_query($conexao, $sql_3);
+        //$sql_3 = "SELECT * FROM notas_observacao WHERE code = '$code' AND disciplina = '$disciplina' AND semestre = '2'";
+        //$result_3 = mysqli_query($conexao, $sql_3);
 
-        $sql_4 = "SELECT * FROM notas_observacao WHERE code = '$code' AND disciplina = '$disciplina' AND semestre = '3'";
-        $result_4 = mysqli_query($conexao, $sql_4);
+        // $sql_4 = "SELECT * FROM notas_observacao WHERE code = '$code' AND disciplina = '$disciplina' AND semestre = '3'";
+        //$result_4 = mysqli_query($conexao, $sql_4);
 
-        $sql_5 = "SELECT * FROM notas_observacao WHERE code = '$code' AND disciplina = '$disciplina' AND semestre = '4'";
-        $result_5 = mysqli_query($conexao, $sql_5);
+        //$sql_5 = "SELECT * FROM notas_observacao WHERE code = '$code' AND disciplina = '$disciplina' AND semestre = '4'";
+        //$result_5 = mysqli_query($conexao, $sql_5);
 
         ?>
             <tr>
@@ -284,51 +268,51 @@ if (mysqli_num_rows($result_2) == '') {
                 </td>
                 <td>
                     <?php
-if (mysqli_num_rows($result_3) == '') {
-            echo "<h2>Aguarda</h2>";
-        } else {
-            while ($res_3 = mysqli_fetch_assoc($result_3)) {
-                $nota = $res_3['nota'];
+//if (mysqli_num_rows($result_3) == '') {
+        //  echo "<h2>Aguarda</h2>";
+        //  } else {
+        //while ($res_3 = mysqli_fetch_assoc($result_3)) {
+        // $nota = $res_3['nota'];
 
-                if ($nota >= 7) {
-                    echo "<h2><strong>" . $res_3['nota'] . "</strong></h2>";
-                } else {
-                    echo "<h3><strong>" . $res_3['nota'] . "</strong></h3>";
-                }
+        // if ($nota >= 7) {
+        //   echo "<h2><strong>" . $res_3['nota'] . "</strong></h2>";
+        //  } else {
+        //  echo "<h3><strong>" . $res_3['nota'] . "</strong></h3>";
+        //   }
 
-            }}?>
+        // }}?>
                 </td>
                 <td>
                     <?php
-if (mysqli_num_rows($result_4) == '') {
-            echo "<h2>Aguarda</h2>";
-        } else {
-            while ($res_4 = mysqli_fetch_assoc($result_4)) {
-                $nota = $res_4['nota'];
+//if (mysqli_num_rows($result_4) == '') {
+        // echo "<h2>Aguarda</h2>";
+        // } else {
+        //  while ($res_4 = mysqli_fetch_assoc($result_4)) {
+        // $nota = $res_4['nota'];
 
-                if ($nota >= 7) {
-                    echo "<h2><strong>" . $res_4['nota'] . "</strong></h2>";
-                } else {
-                    echo "<h3><strong>" . $res_4['nota'] . "</strong></h3>";
-                }
+        // if ($nota >= 7) {
+        // echo "<h2><strong>" . $res_4['nota'] . "</strong></h2>";
+        //  } else {
+        //echo "<h3><strong>" . $res_4['nota'] . "</strong></h3>";
+        // }
 
-            }}?>
+        //  }}?>
                 </td>
                 <td>
                     <?php
-if (mysqli_num_rows($result_5) == '') {
-            echo "<h2>Aguarda</h2>";
-        } else {
-            while ($res_5 = mysqli_fetch_assoc($result_5)) {
-                $nota = $res_5['nota'];
+//if (mysqli_num_rows($result_5) == '') {
+        // echo "<h2>Aguarda</h2>";
+        // } else {
+        //  while ($res_5 = mysqli_fetch_assoc($result_5)) {
+        //  $nota = $res_5['nota'];
 
-                if ($nota >= 7) {
-                    echo "<h2><strong>" . $res_5['nota'] . "</strong></h2>";
-                } else {
-                    echo "<h3><strong>" . $res_5['nota'] . "</strong></h3>";
-                }
+        //if ($nota >= 7) {
+        // echo "<h2><strong>" . $res_5['nota'] . "</strong></h2>";
+        //  } else {
+        // echo "<h3><strong>" . $res_5['nota'] . "</strong></h3>";
+        // }
 
-            }}?>
+        // }}?>
                 </td>
             </tr>
             <?php }?>
@@ -345,9 +329,9 @@ if (mysqli_num_rows($result_5) == '') {
         <table width="900" border="0">
             <tr>
                 <td width="317"><strong>DISCIPLINA<br /><br /></strong></td>
-                <td width="120"><strong>1º Semestre</strong></td>
+                <!-- <td width="120"><strong>1º Semestre</strong></td>
                 <td width="120"><strong>2º Semestre</strong></td>
-                <!--<td width="120"><strong>3º Bimestre</strong></td>
+                <td width="120"><strong>3º Bimestre</strong></td>
     <td width="120"><strong>4º Bimestre</strong></td>
     <td width="150"><strong>Resultado</strong></td>-->
             </tr>
@@ -360,14 +344,14 @@ $sql_1 = "SELECT * FROM disciplinas WHERE curso = '$serie'";
         $sql_2 = "SELECT * FROM notas_bimestrais WHERE code = '$code' AND disciplina = '$disciplina' AND semestre = '1'";
         $result_2 = mysqli_query($conexao, $sql_2);
 
-        $sql_3 = "SELECT * FROM notas_bimestrais WHERE code = '$code' AND disciplina = '$disciplina' AND semestre = '2'";
-        $result_3 = mysqli_query($conexao, $sql_3);
+        //  $sql_3 = "SELECT * FROM notas_bimestrais WHERE code = '$code' AND disciplina = '$disciplina' AND semestre = '2'";
+        // $result_3 = mysqli_query($conexao, $sql_3);
 
-        $sql_4 = "SELECT * FROM notas_bimestrais WHERE code = '$code' AND disciplina = '$disciplina' AND semestre = '3'";
-        $result_4 = mysqli_query($conexao, $sql_4);
+        // $sql_4 = "SELECT * FROM notas_bimestrais WHERE code = '$code' AND disciplina = '$disciplina' AND semestre = '3'";
+        // $result_4 = mysqli_query($conexao, $sql_4);
 
-        $sql_5 = "SELECT * FROM notas_bimestrais WHERE code = '$code' AND disciplina = '$disciplina' AND semestre = '4'";
-        $result_5 = mysqli_query($conexao, $sql_5);
+        // $sql_5 = "SELECT * FROM notas_bimestrais WHERE code = '$code' AND disciplina = '$disciplina' AND semestre = '4'";
+        // $result_5 = mysqli_query($conexao, $sql_5);
 
         ?>
             <tr>
@@ -391,86 +375,86 @@ if (mysqli_num_rows($result_2) == '') {
                 </td>
                 <td>
                     <?php
-if (mysqli_num_rows($result_3) == '') {
-            echo "<h2>Aguarda</h2>";
-        } else {
-            while ($res_3 = mysqli_fetch_assoc($result_3)) {
-                $nota = number_format($res_3['nota'], 2);
+//if (mysqli_num_rows($result_3) == '') {
+        // echo "<h2>Aguarda</h2>";
+        // } else {
+        //  while ($res_3 = mysqli_fetch_assoc($result_3)) {
+        // $nota = number_format($res_3['nota'], 2);
 
-                if ($nota >= 7) {
-                    echo "<h2><strong>$nota</strong></h2>";
-                } else {
-                    echo "<h3><strong>$nota</strong></h3>";
-                }
+        // if ($nota >= 7) {
+        //   echo "<h2><strong>$nota</strong></h2>";
+        // } else {
+        //     echo "<h3><strong>$nota</strong></h3>";
+        //  }
 
-            }
-        }?>
+        //  }
+        //  }?>
                 </td>
                 <td>
                     <?php
-if (mysqli_num_rows($result_4) == '') {
-            echo "<h2>Aguarda</h2>";
-        } else {
-            while ($res_4 = mysqli_fetch_assoc($result_4)) {
-                $nota = number_format($res_4['nota'], 2);
+//if (mysqli_num_rows($result_4) == '') {
+        //   echo "<h2>Aguarda</h2>";
+        // } else {
+        // while ($res_4 = mysqli_fetch_assoc($result_4)) {
+        //  $nota = number_format($res_4['nota'], 2);
 
-                if ($nota >= 7) {
-                    echo "<h2><strong>$nota</strong></h2>";
-                } else {
-                    echo "<h3><strong>$nota</strong></h3>";
-                }
+        //  if ($nota >= 7) {
+        //   echo "<h2><strong>$nota</strong></h2>";
+        // } else {
+        //  echo "<h3><strong>$nota</strong></h3>";
+        // }
 
-            }
-        }?>
+        // }
+        // }?>
                 </td>
                 <td>
                     <?php
-if (mysqli_num_rows($result_5) == '') {
-            echo "<h2>Aguarda</h2>";
-        } else {
-            while ($res_5 = mysqli_fetch_assoc($result_5)) {
-                $nota = number_format($res_5['nota'], 2);
+//if (mysqli_num_rows($result_5) == '') {
+        // echo "<h2>Aguarda</h2>";
+        // } else {
+        //while ($res_5 = mysqli_fetch_assoc($result_5)) {
+        //  $nota = number_format($res_5['nota'], 2);
 
-                if ($nota >= 7) {
-                    echo "<h2><strong>$nota</strong></h2>";
-                } else {
-                    echo "<h3><strong>$nota</strong></h3>";
-                }
+        // if ($nota >= 7) {
+        // echo "<h2><strong>$nota</strong></h2>";
+        //  } else {
+        //echo "<h3><strong>$nota</strong></h3>";
+        // }
 
-            }
-        }?>
+        //}
+        //}?>
                 </td>
                 <td>
                     <?php
-if (mysqli_num_rows($result_5) == '') {
-            echo "Aguarda";
-        } else {
-            $sql_2 = "SELECT * FROM notas_bimestrais WHERE code = '$code' AND disciplina = '$disciplina' AND semestre = '1'";
-            $result_2 = mysqli_query($conexao, $sql_2);
+//if (mysqli_num_rows($result_5) == '') {
+        // echo "Aguarda";
+        // } else {
+        //  $sql_2 = "SELECT * FROM notas_bimestrais WHERE code = '$code' AND disciplina = '$disciplina' AND semestre = '1'";
+        // $result_2 = mysqli_query($conexao, $sql_2);
 
-            $sql_3 = "SELECT * FROM notas_bimestrais WHERE code = '$code' AND disciplina = '$disciplina' AND semestre = '2'";
-            $result_3 = mysqli_query($conexao, $sql_3);
+        // $sql_3 = "SELECT * FROM notas_bimestrais WHERE code = '$code' AND disciplina = '$disciplina' AND semestre = '2'";
+        // $result_3 = mysqli_query($conexao, $sql_3);
 
-            $sql_4 = "SELECT * FROM notas_bimestrais WHERE code = '$code' AND disciplina = '$disciplina' AND semestre = '3'";
-            $result_4 = mysqli_query($conexao, $sql_4);
+        // $sql_4 = "SELECT * FROM notas_bimestrais WHERE code = '$code' AND disciplina = '$disciplina' AND semestre = '3'";
+        // $result_4 = mysqli_query($conexao, $sql_4);
 
-            $sql_5 = "SELECT * FROM notas_bimestrais WHERE code = '$code' AND disciplina = '$disciplina' AND semestre = '4'";
-            $result_5 = mysqli_query($conexao, $sql_5);
+        //  $sql_5 = "SELECT * FROM notas_bimestrais WHERE code = '$code' AND disciplina = '$disciplina' AND semestre = '4'";
+        //  $result_5 = mysqli_query($conexao, $sql_5);
 
-            while ($res_2 = mysqli_fetch_assoc($result_2)) {
-                while ($res_3 = mysqli_fetch_assoc($result_3)) {
-                    while ($res_4 = mysqli_fetch_assoc($result_4)) {
-                        while ($res_5 = mysqli_fetch_assoc($result_5)) {
+        //  while ($res_2 = mysqli_fetch_assoc($result_2)) {
+        // while ($res_3 = mysqli_fetch_assoc($result_3)) {
+        //   while ($res_4 = mysqli_fetch_assoc($result_4)) {
+        //  while ($res_5 = mysqli_fetch_assoc($result_5)) {
 
-                            $media = ($res_2['nota'] + $res_3['nota'] + $res_4['nota'] + $res_5['nota']) / 4;
+        //  $media = ($res_2['nota'] + $res_3['nota'] + $res_4['nota'] + $res_5['nota']) / 4;
 
-                            if ($media >= 7) {
-                                echo "<h2><strong>" . number_format($media, 2) . " - Aprovado</strong></h2>";
-                            } else {
-                                echo "<h3><strong>" . number_format($media, 2) . " - Reprovado</strong></h3>";
+        // if ($media >= 7) {
+        //    echo "<h2><strong>" . number_format($media, 2) . " - Aprovado</strong></h2>";
+        // } else {
+        //  echo "<h3><strong>" . number_format($media, 2) . " - Reprovado</strong></h3>";
 
-                            }}}}}}
-        ?>
+        // }}}}}}
+        //?>
                 </td>
             </tr>
             <?php }?>
